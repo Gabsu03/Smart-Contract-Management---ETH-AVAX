463ab5fbdf6a42dafb41f492ff17836d312a9f4f
Gabsu03 <128680936+Gabsu03@users.noreply.github.com>
2024-05-27 12:31:08 +0800

    Update Assessment.sol

--- Assessment.sol
+++ Assessment.sol
@@ -1,60 +1,53 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-//import "hardhat/console.sol";
+// SPDX-License-Identifier: GPL-3.0 pragma solidity 0.8.13;
 
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+    address payable public galera;
+    uint256 public amount;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    constructor(uint initAmount) payable {
+        galera = payable(msg.sender);
+        amount = initAmount;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    function getamount() public view returns(uint256){
+        return amount;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+        uint _previousBalance = amount;
 
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
+        require(msg.sender == galera, "You are not the owner of this account");
 
-        // perform transaction
-        balance += _amount;
+        amount += _amount;
 
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
+        assert(amount == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
+    
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
+        require(msg.sender == galera, "You are not the owner of this account");
+        uint _previousBalance = amount;
+        if (amount < _withdrawAmount) {
             revert InsufficientBalance({
-                balance: balance,
+                balance: amount,
                 withdrawAmount: _withdrawAmount
             });
         }
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
+        
+        amount -= _withdrawAmount;
 
-        // assert the balance is correct
+        
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
+        
         emit Withdraw(_withdrawAmount);
     }
 }
